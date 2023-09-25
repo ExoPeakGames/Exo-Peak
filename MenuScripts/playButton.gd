36fde7b0bbfd96dad948ac4ed9490324eaa9cce5
PureAsbestos <16106576+PureAsbestos@users.noreply.github.com>
2023-09-25 14:34:56 -0500

    Remove MenuScripts folder

--- MenuScripts/playButton.gd
+++
@@ -1,4 +0,0 @@
-extends TextureButton
-
-func _on_playButton_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
--- MenuScripts/returnButton.gd
+++
@@ -1,4 +0,0 @@
-extends TextureButton
-
-func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/title_screen.tscn") # Replace with function body.
