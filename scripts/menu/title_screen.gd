5ca3253438e616f9945120f2dc99d1db151c1e4c
svg190000 <svg190000@utdallas.edu>
2023-11-01 13:47:53 -0600

    Refactor menu system

---
+++ scripts/menu/title_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_button_pressed():
+	MenuButtons._on_play_button_pressed()
+
+func _on_settings_button_pressed():
+	MenuButtons._on_settings_button_pressed()
+
+func _on_achievements_button_pressed():
+	MenuButtons._on_achievements_button_pressed()
