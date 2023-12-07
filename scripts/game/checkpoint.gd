3be83bc9089743b2dded14c646063279a928f811
svg190000 <97975415+svg190000@users.noreply.github.com>
2023-12-07 17:39:39 -0600

    delete nodes on scene1

--- scripts/game/checkpoint.gd
+++ scripts/game/checkpoint.gd
@@ -1,6 +1,20 @@
 extends Area2D
 
+var playerSave: PlayerSave
+var newGame: NewGame
+var gameMode
+
+func _ready():
+	if PlayerData:
+		newGame = NewGame.load_or_create()
+		playerSave = PlayerSave.load_or_create()
+		gameMode = newGame
+	else:
+		gameMode = playerSave
+	
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		PlayerData.spawnerLocation = position
+		gameMode.position = position
+		playerSave.save()
 		print("chek")
--- scripts/menu/title_screen.gd
+++ scripts/menu/title_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	MenuButtons._on_play_button_pressed()
+	$"LeftPanel/VBoxContainer/PlayButton/playModes".show()
 
 func _on_settings_button_pressed():
 	MenuButtons._on_settings_button_pressed()
@@ -11,3 +11,16 @@ func _on_achievements_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_new_game_pressed():
+	PlayerData.new_game = true
+	MenuButtons._on_play_button_pressed()
+
+func _on_load_game_pressed():
+	PlayerData.new_game = false
+	MenuButtons._on_play_button_pressed()
+
+func _on_back_button_pressed():
+	$"LeftPanel/VBoxContainer/PlayButton/playModes".hide()
+
+
