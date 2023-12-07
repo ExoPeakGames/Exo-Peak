e6d8b8783201f452054043489dcca5ada868611f
PureAsbestos <16106576+PureAsbestos@users.noreply.github.com>
2023-12-07 14:03:36 -0600

    Remove unnecessary code

--- scripts/game/scene_1.gd
+++ scripts/game/scene_1.gd
@@ -1,9 +1,2 @@
 extends Control
 
-var user_pref: UserPreferences
-
-func _ready():
-	user_pref = UserPreferences.load_or_create()
-	AudioServer.set_bus_volume_db(0, linear_to_db(user_pref.master))
-	AudioServer.set_bus_volume_db(1, linear_to_db(user_pref.music))
-	AudioServer.set_bus_volume_db(2, linear_to_db(user_pref.sfx))
